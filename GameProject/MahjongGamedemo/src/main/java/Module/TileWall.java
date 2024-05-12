3ca9a80703445c51896fa182be2ce188bd45a0d3
laijiewo <jingwang.li@ucdconnect.ie>
2024-05-12 18:16:27 +0800

    add javaFX lib

--- GameProject/MahjongGamedemo/src/main/java/Module/TileWall.java
+++ GameProject/MahjongGamedemo/src/main/java/Module/TileWall.java
@@ -2,10 +2,11 @@ package Module;
 import System.*;
 import Display.*;
 
-import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
 
 
 public class TileWall {
-    ArrayList<Tile> StackOfTiles = new ArrayList<Tile>();
+    List<Tile> StackOfTiles = new Stack<>();
     public void buildTheWall(){}
 }
