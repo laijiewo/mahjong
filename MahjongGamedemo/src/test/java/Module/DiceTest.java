60758d2f5e7c5a523fb8b942b9aba58ac2f0c931
Nancy-muchen <nyx4150445@163.com>
2024-05-13 22:02:19 +0800

    Update Test Dice

--- MahjongGamedemo/src/test/java/Module/DiceTest.java
+++ MahjongGamedemo/src/test/java/Module/DiceTest.java
@@ -8,9 +8,22 @@ public class DiceTest {
 
     @Test
     public void toss() {
+        Dice dice=new Dice();
+        int pips=dice.toss();
+        assertTrue("Pips should be in the range of 1 to 6", pips >= 1 && pips <= 6);
     }
 
     @Test
     public void randomPip() {
+        Dice dice= new Dice();
+        int pips=dice.randomPip();
+        assertTrue("Pips should be in the range of 1 to 6", pips >= 1 && pips <= 6);
+    }
+    @Test
+    public void randomPip_shouldGenerateRandomPips() {
+        Dice dice = new Dice();
+        int pips1 = dice.randomPip();
+        int pips2 = dice.randomPip();
+        assertNotEquals("Random pips should not be the same", pips1, pips2);
     }
 }
