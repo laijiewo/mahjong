859c280a3ddc9e1e30d7cbc788d8f15c86aa2e21
Nancy-muchen <nyx4150445@163.com>
2024-05-13 21:00:18 +0800

    complete and create
    complete the Dice class and create a class to implement the rules

--- MahjongGamedemo/src/main/java/Module/Dice.java
+++ MahjongGamedemo/src/main/java/Module/Dice.java
@@ -2,15 +2,37 @@ package Module;
 
 import java.util.Random;
 
+/**
+ * This class represents a six-sided dice that can be tossed to get a random outcome.
+ */
 public class Dice {
-    private int pips;
-    private Random rand = new Random();
+    private int pips; // Number of pips facing up after tossing the dice
+    private Random rand; // Random number generator used for tossing the dice
+
+    /**
+     * Constructs a new Dice object with a random number generator initialized.
+     */
     public Dice(){
+        rand = new Random();
     }
-    public int randomPip(){
-        return rand.nextInt(6);
-    }
-    public int getPips() {
+
+    /**
+     * Tosses the dice and returns the result.
+     *
+     * @return The number of pips facing up after tossing the dice.
+     */
+    public int toss(){
+        pips = randomPip();
         return pips;
     }
+
+    /**
+     * Generates a random number representing the pips on the dice face (1 to 6).
+     *
+     * @return A random integer representing the pips on the dice face.
+     */
+    public int randomPip(){
+        return rand.nextInt(6)+1;
+    }
 }
+
