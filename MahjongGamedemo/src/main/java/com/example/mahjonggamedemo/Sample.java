89ba512f72301a55f8db8b773a8d2fdb9af9f3b8
Eli-Mao <1426635781@qq.com>
2024-05-15 11:06:52 +0800

    Sample.fxml测试

--- MahjongGamedemo/src/main/java/com/example/mahjonggamedemo/Sample.java
+++ MahjongGamedemo/src/main/java/com/example/mahjonggamedemo/Sample.java
@@ -1,4 +1,26 @@
 package com.example.mahjonggamedemo;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class Sample {
+
+    @FXML
+    private TextField AccountText;
+
+    @FXML
+    private TextField PasswordText;
+
+    @FXML
+    private Label AccountLabel;
+
+    @FXML
+    private Button LogininButton;
+
+    @FXML
+    private Label PasswordLabel;
+
 }
+
