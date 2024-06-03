8c3dda7f26616c9efc3814bb5504cb7516624ede
Eli-Mao <1426635781@qq.com>
2024-06-03 18:52:34 +0800

    settlementScreen

---
+++ MahjongGamedemo/src/main/java/Display/GameScreenDisplay/SettlementScreen.java
@@ -0,0 +1,13 @@
+package Display.GameScreenDisplay;
+import Display.GameScreenDisplay.ButtonDisplay;
+import Display.Screen;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Background;
+import javafx.stage.Stage;
+
+public class SettlementScreen implements Screen {
+    @Override
+    public void loadWindow(Stage stage) throws Exception {
+
+    }
+}
