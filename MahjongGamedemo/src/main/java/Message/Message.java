e131a7fcfa84e0b998176808fbdc24711f7e69f6
Eli-Mao <1426635781@qq.com>
2024-06-08 23:29:27 +0800

    Reapply "update doc explanation"
    This reverts commit 5788716c925559740afe45dcb420d7a33237d4cb.

--- MahjongGamedemo/src/main/java/Message/Message.java
+++ MahjongGamedemo/src/main/java/Message/Message.java
@@ -2,21 +2,32 @@ package Message;
 
 import java.io.Serial;
 import java.io.Serializable;
-import java.util.List;
 
-import Module.Game.MahjongGame;
-import Module.Tile.Tile;
-
+/**
+ * Represents a general message in the Mahjong game.
+ * Implements Serializable for message serialization.
+ */
 public class Message implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
     private MessageType type;
 
-    // HU, PAUSE and UNPAUSE Message
+    /**
+     * Constructs a new Message with the specified type.
+     *
+     * @param type The type of the message.
+     */
     public Message(MessageType type) {
         this.type = type;
     }
+
+    /**
+     * Gets the type of the message.
+     *
+     * @return The type of the message.
+     */
     public MessageType getType() {
         return type;
     }
 }
+
