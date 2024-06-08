4e79b6672778b4bf8bb8940d3da9c9187cce8deb
Eli-Mao <1426635781@qq.com>
2024-06-08 23:10:44 +0800

    Revert "update"
    This reverts commit 8ad62a86be2dacf8837bbca26b1771f8b56d2801.

--- MahjongGamedemo/src/main/java/Display/JoinScreen.java
+++ MahjongGamedemo/src/main/java/Display/JoinScreen.java
@@ -23,56 +23,47 @@ public class JoinScreen implements Screen {
 
     @FXML
     private TextField PortText;
-
-    /**
-     * Handles the action of joining a game. This method is triggered when the user clicks the "Join Game" button.
-     * It attempts to connect the player to the server using the IP address and port specified in the text fields.
-     *
-     * @param event The action event triggered by clicking the "Join Game" button.
-     */
     @FXML
     void JoinGame(ActionEvent event) {
         Player player = new Player();
         player.joinToHost();
         String ip = IpText.getText();
         int port = Integer.parseInt(PortText.getText());
-
-        if (!ip.isEmpty() && port != 0) {
+        if(ip!="" && port!=0){
             player.setServerHostname(ip);
             player.setServerPort(port);
-
             try {
                 player.connect();
                 if (player.getconnected()) {
-                    showAlert(Alert.AlertType.INFORMATION, "Connected to Server", "You are connected to the server");
+                    Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                    alert.setTitle("Connected to Server");
+                    alert.setHeaderText("Connected to Server");
+                    alert.setContentText("You are connected to the server");
+                    alert.showAndWait();
                     player.joinToHost();
                 } else {
-                    showAlert(Alert.AlertType.ERROR, "Error", "You are not connected to the server");
+                    Alert alert = new Alert(Alert.AlertType.ERROR);
+                    alert.setTitle("Error");
+                    alert.setHeaderText("Error");
+                    alert.setContentText("You are not connected to the server");
+                    alert.showAndWait();
                 }
-            } catch (Exception e) {
-                showAlert(Alert.AlertType.ERROR, "Error", "You are not connected to the server");
+            }catch (Exception e){
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error");
+                alert.setHeaderText("Error");
+                alert.setContentText("You are not connected to the server");
+                alert.showAndWait();
                 e.printStackTrace();
             }
         }
     }
-
-    /**
-     * Closes the current window.
-     *
-     * @return The current instance of JoinScreen.
-     */
     public JoinScreen closeWindow() {
         Stage stage = (Stage) JoinGameButton.getScene().getWindow();
         stage.close();
         return this;
     }
 
-    /**
-     * Loads the join screen window.
-     *
-     * @param stage The primary stage for this application.
-     * @throws Exception if there is an error loading the FXML file.
-     */
     @Override
     public void loadWindow(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/Display/JoinScreen.fxml"));
@@ -81,21 +72,5 @@ public class JoinScreen implements Screen {
         stage.setTitle("Join Screen");
         stage.show();
     }
-
-    /**
-     * Shows an alert dialog with the specified type, title, and content.
-     *
-     * @param alertType The type of alert (e.g., INFORMATION, ERROR).
-     * @param title The title of the alert dialog.
-     * @param content The content text of the alert dialog.
-     */
-    private void showAlert(Alert.AlertType alertType, String title, String content) {
-        Alert alert = new Alert(alertType);
-        alert.setTitle(title);
-        alert.setHeaderText(title);
-        alert.setContentText(content);
-        alert.showAndWait();
-    }
 }
-
 
--- MahjongGamedemo/src/main/java/Display/LaunchScreen.java
+++ MahjongGamedemo/src/main/java/Display/LaunchScreen.java
@@ -22,20 +22,13 @@ public class LaunchScreen implements Screen {
 
     @FXML
     private TextField PortText;
-
     @FXML
     private Button StartGameButton;
 
     @FXML
     private Label Waiting;
 
-    /**
-     * Handles the action of starting a new game. This method is triggered when the user clicks the "Start Game" button.
-     * It opens the GameScreen window and closes the current window.
-     *
-     * @param event The action event triggered by clicking the "Start Game" button.
-     * @throws Exception if there is an error loading the GameScreen window.
-     */
+
     @FXML
     void StartGame(ActionEvent event) throws Exception {
         GameScreen gameScreen = new GameScreen();
@@ -44,45 +37,25 @@ public class LaunchScreen implements Screen {
         stage.close();
     }
 
-    /**
-     * Handles the action of launching a game server. This method is triggered when the user clicks the "Launch Game" button.
-     * It creates a new game server at the specified port and displays a success alert.
-     *
-     * @param event The action event triggered by clicking the "Launch Game" button.
-     * @throws IOException if there is an error creating the game server.
-     */
     @FXML
     void LaunchGame(ActionEvent event) throws IOException {
         int port = Integer.parseInt(PortText.getText());
-        if (port != 0) {
+        if(port!=0){
             Alert alert = new Alert(Alert.AlertType.INFORMATION);
             alert.setTitle("Server Host");
             alert.setHeaderText("Create Server Host");
-            alert.setContentText("Successfully created game at server Port: " + port);
-            alert.showAndWait();
-
+            alert.setContentText("Successfully create game at server Port: "+port);
             MahjongGame game = new MahjongGame(port);
             GameManager.addGame(game);
         }
     }
 
-    /**
-     * Closes the current window.
-     *
-     * @return The current instance of LaunchScreen.
-     */
     public LaunchScreen closeWindow() {
         Stage stage = (Stage) LaunchGameButton.getScene().getWindow();
         stage.close();
         return this;
     }
 
-    /**
-     * Loads the launch screen window.
-     *
-     * @param stage The primary stage for this application.
-     * @throws Exception if there is an error loading the FXML file.
-     */
     public void loadWindow(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/Display/LaunchScreen.fxml"));
         Scene scene = new Scene(root);
@@ -91,5 +64,4 @@ public class LaunchScreen implements Screen {
         stage.show();
     }
 }
-
 
