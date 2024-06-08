52fcf7e36764b67faa63212b1855c7889c82d216
Eli-Mao <1426635781@qq.com>
2024-06-08 23:00:51 +0800

    update doc explanation

--- MahjongGamedemo/src/main/java/Message/joinMessage.java
+++ MahjongGamedemo/src/main/java/Message/joinMessage.java
@@ -2,8 +2,18 @@ package Message;
 
 import Module.Game.Player;
 
+
+/**
+ * Represents a message for a join action in the Mahjong game.
+ * Extends the base Message class.
+ */
 public class joinMessage extends Message {
+
+    /**
+     * Constructs a new JoinMessage.
+     */
     public joinMessage() {
         super(MessageType.JOIN);
     }
 }
+
